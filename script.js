0cd829ca3064cc9394e7276f3688f9fd1878b1fc
nick32007 <kobets32@gmail.com>
2022-04-06 15:37:01 +0300

    dz06

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log('hi'); 
+ 
+const isNumber = function(num) {
+  return !isNaN(parseFloat(num)) && isFinite(num);
+}
+
+
+function one(num) {
+   function two() {
+      let a = prompt('Угадай число от 1 до 100');
+
+       if(a === null) {
+        alert("Игра окончена");
+       } else if(!isNumber(a)) {
+        alert("Введи число!"); 
+        two();
+       } else if(a == num) {
+        alert("Поздравляю, Вы угадали!!!"); 
+       } else if(num > a) {
+        alert("Загаданное число больше");
+        two();
+       } else if(num < a) {
+        alert("Загаданное число меньше");
+        two();
+      }
+  }
+ two();
+ 
+}
+
+one(50);
+
+
